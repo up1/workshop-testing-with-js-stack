3af32748a1615e2b5533566dc627799c5f90f8e6
up1 <somkiat.p@gmail.com>
2024-12-25 23:38:08 +0700

    Add ui

--- frontend/vue-project/src/services/api.ts
+++ frontend/vue-project/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000', // Replace with your actual API base URL
+  baseURL: 'http://localhost:8888/api', // Replace with your actual API base URL
   headers: {
     'Content-Type': 'application/json',
   },
